183b0eb599d9dfbbd63564c8a005042aaf6410e6
Ishika Goyal <goyalishika132@gmail.com>
2024-05-26 13:45:02 +0530

    new component about is added

--- port/src/components/Intro/intro.js
+++ port/src/components/Intro/intro.js
@@ -10,15 +10,26 @@ const Intro = () =>
                 <div className="introContent">
                 <span className="hello">Hello,</span>
                 <br></br>
-                <span className="introText">I'm <span className="introName">Ishika</span><br/>Website Designer</span>
+                <span className="introText">I'm <span className="introName">Ishika</span><br/>
+                {/* <ul className="dynamic">
+                    <li><span> Web Developer</span></li>
+                    <li><span> Open Source Enthusiast</span></li>
+                    <li><span> Problem Solver</span></li>
+                    <li><span> Website Designer</span></li>
+                </ul> */}
+                
+                Web Developer
+                </span>
 
                 <p className="para">I am a skilled web designer with experience in creating visually appealing and user friendly website</p>
                 
-                <Link><button className="btn">Download Resume</button></Link>
+                <a href={require("../../assets/IshikaGoyal_B.TechCS.pdf")} download="Ishika_Resume.pdf">
+                    <button className="btn">Download Resume</button>
+                </a>
                 
                 </div>
                 <img src={bg} alt='Profile' className="bg"/>
-            
+                
 
             </section>
 
