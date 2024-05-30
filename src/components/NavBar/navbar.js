de8982ef00ea06190094cdddd7ead1d6a2fbf988
Ishika Goyal <goyalishika132@gmail.com>
2024-05-30 15:27:29 +0530

    centralized the skills component

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -9,14 +9,13 @@ const NavBar = ()=>
             <nav className="navlist">
             {/* <img src={logo} alt="LOGO" className="logo"></img> */}
            
-            <Link to="/" smooth={true} duration={1000} className="navButton">HOME</Link>
             <Link to="intro" smooth={true} duration={1000} className="navButton">HOME</Link>
             <Link to="about" smooth={true} duration={1000} className="navButton">ABOUT ME</Link>
             <Link to="skills" smooth={true} duration={1000} className="navButton">SKILLS</Link>
             <Link to="education" smooth={true} duration={1000} className="navButton">EDUCATION</Link>
             <Link to="experience" smooth={true} duration={1000} className="navButton">EXPERIENCE</Link>
             <Link to="projects" smooth={true} duration={1000} className="navButton">PROJECTS</Link>
-            <Link to="certificates" smooth={true} duration={1000} className="navButton">CERTIFICATES</Link>
+            {/* <Link to="certificates" smooth={true} duration={1000} className="navButton">CERTIFICATES</Link> */}
             <Link to="contact" smooth={true} duration={1000} className="navButton">CONTACT</Link>
             </nav> 
         )
