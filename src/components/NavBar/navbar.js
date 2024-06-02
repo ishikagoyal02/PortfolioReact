e5b36cf27aec151abbbe32a9e1d0a4fcd2cbabe8
Ishika Goyal <goyalishika132@gmail.com>
2024-06-02 20:08:27 +0530

    image size and color changed in mobile view

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,29 +1,6 @@
-// import React from "react";
-// import './navbar.css';
-//  import logo from '../../assets/logo.jpg';
-//  import { Link } from 'react-scroll';
-
-// const NavBar = ()=>
-//     {
-//         return (
-//             <nav className="navlist">
-//             <Link to="intro" smooth={true} duration={1000} className="navButton">HOME</Link>
-//             <Link to="about" smooth={true} duration={1000} className="navButton">ABOUT ME</Link>
-//             <Link to="skills" smooth={true} duration={1000} className="navButton">SKILLS</Link>
-//             <Link to="education" smooth={true} duration={1000} className="navButton">EDUCATION</Link>
-//             <Link to="experience" smooth={true} duration={1000} className="navButton">EXPERIENCE</Link>
-//             <Link to="projects" smooth={true} duration={1000} className="navButton">PROJECTS</Link>
-//             <Link to="contact" smooth={true} duration={1000} className="navButton">CONTACT</Link>
-//             </nav> 
-//         )
-//     }
-
-//     export default NavBar
-
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 import './navbar.css';
-// import logo from '../../assets/logo.jpg';
 
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -33,8 +10,7 @@ const NavBar = () => {
     };
 
     return (
-        <nav className="navlist">
-            {/* <img src={logo} alt="LOGO" className="logo"></img> */}
+        <nav className={`navlist ${isOpen ? "open" : ""}`}>
             <button className="menuButton" onClick={toggleMenu}>
                 ☰
             </button>
@@ -45,7 +21,6 @@ const NavBar = () => {
                 <Link to="education" smooth={true} duration={1000} className="navButton" onClick={toggleMenu}>EDUCATION</Link>
                 <Link to="experience" smooth={true} duration={1000} className="navButton" onClick={toggleMenu}>EXPERIENCE</Link>
                 <Link to="projects" smooth={true} duration={1000} className="navButton" onClick={toggleMenu}>PROJECTS</Link>
-                {/* <Link to="certificates" smooth={true} duration={1000} className="navButton">CERTIFICATES</Link> */}
                 <Link to="contact" smooth={true} duration={1000} className="navButton" onClick={toggleMenu}>CONTACT</Link>
             </div>
         </nav>
