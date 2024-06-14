aeda46a10b48c0f38fdc4dd67038088ec02c93fd
Ishika Goyal <goyalishika132@gmail.com>
2024-06-14 21:35:09 +0530

    about component styling changed

--- src/components/AboutUs/about.js
+++ src/components/AboutUs/about.js
@@ -4,9 +4,7 @@ import React from "react";
 import './about.css';
 import pp from "../../assets/pp2.jpg";
 import { Link } from "react-scroll";
-import linkedln from "../../assets/linkedln.png";
-import github from "../../assets/github.png";
-import linktree from "../../assets/linktree.png";
+
 
 const About = () => {
   return (
@@ -26,20 +24,7 @@ const About = () => {
              <br/> <br/> My short-term goal is to gain practical experience in software development through internships and collaborative projects, enhancing my technical and professional skills. My long-term goal is to become a lead software engineer, contributing to innovative projects that make a significant impact in the tech industry, and eventually mentor and guide aspiring developers.
            </span>
  
-           <div className="contacts">
-              <a href="https://linktr.ee/ishika_goyal02" target="_blank" rel="noopener noreferrer">
-              <img src={linktree} className="cont linktree" alt="Linktree" />
-            </a>
-            
-            <a href="https://www.linkedin.com/in/ishika-goyal-375112221/" target="_blank" rel="noopener noreferrer">
-              <img src={linkedln} className="cont linkedin" alt="LinkedIn" />
-            </a>
-            
-            <a href="https://github.com/ishikagoyal02" target="_blank" rel="noopener noreferrer">
-              <img src={github} className="cont github" alt="GitHub" />
-            </a>
-             
-           </div>
+           
          </div>
          </div>
       </section>
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -4,6 +4,9 @@ import bg from '../../assets/bg.png';
 import { Link } from 'react-scroll';
 import { TypeAnimation } from 'react-type-animation';
 import logoHome from '../../assets/logoHome.mp4';
+import linkedln from "../../assets/linkedln.png";
+import github from "../../assets/github.png";
+import linktree from "../../assets/linktree.png";
 
 const Intro = () => {
     return (
@@ -22,6 +25,21 @@ const Intro = () => {
                 <a href={require("../../assets/IshikaGoyal_B.TechCS.pdf")} download="Ishika_Resume.pdf">
                     <button className="btn">Download Resume</button>
                 </a>
+
+                <div className="contacts">
+              <a href="https://linktr.ee/ishika_goyal02" target="_blank" rel="noopener noreferrer">
+              <img src={linktree} className="cont linktree" alt="Linktree" />
+            </a>
+            
+            <a href="https://www.linkedin.com/in/ishika-goyal-375112221/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedln} className="cont linkedin" alt="LinkedIn" />
+            </a>
+            
+            <a href="https://github.com/ishikagoyal02" target="_blank" rel="noopener noreferrer">
+              <img src={github} className="cont github" alt="GitHub" />
+            </a>
+             
+           </div>
                
             </div>
            
